e019750a84e6a8ba0c197faa221a95ea5bf1209b
Emanuel <emar092009@gmail.com>
2026-07-08 22:23:31 -0500

    tiimer app and files organizaer call

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from PySide6 import QtCore
 import sys
 from PySide6.QtWidgets import (QApplication)
 from UI.MainWindow import MainWindow
+import UI.timmer_app # IMPORTANTE: Se importa aquí para inicializar el timer en el Hilo Principal
+import tools.files_watcher # IMPORTANTE: Se importa para inicializar señales de popups en Hilo Principal
+import tools.files_watcher as flw
+
 
 if __name__ == "__main__":
+    flw.files_watcher_main()
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
